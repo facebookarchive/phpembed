2e94d7274c5d922dae71a501dd8ce374d21ca5fb
mvaradachari <mvaradachari@2c7ba8d8-a2f7-0310-a573-de162e16dcc7>
2007-09-24 07:59:09 +0000

    updated bootstrap.sh. Added README, LICENSE etc
    git-svn-id: svn+ssh://tubbs/svnroot/projects/phpembed/trunk@60856 2c7ba8d8-a2f7-0310-a573-de162e16dcc7

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,12 +3,15 @@
 
 # Script to set up compilation environment.
 
+touch AUTHORS ChangeLog
+
 LIBTOOLIZE=${LIBTOOLIZE:-`which libtoolize`}
 if test -f "${LIBTOOLIZE}"; then
    echo "libtoolize..."
-   $LIBTOOLIZE --automake
+   $LIBTOOLIZE --automake -c
 else
-   echo " skipping libtoolize"
+   echo "Error libtoolize missing. Must install libtools package."
+   exit 1
 fi
 
 echo "aclocal..."
@@ -21,11 +24,10 @@ $AUTOHEADER || exit 1
 
 echo "automake..."
 AUTOMAKE=${AUTOMAKE:-automake}
-$AUTOMAKE -a
+$AUTOMAKE -ac
 
 echo "autoconf..."
 AUTOCONF=${AUTOCONF:-autoconf}
 $AUTOCONF || exit 1
 
-echo "running make clean ....."
 make clean 1>/dev/null 2>&1
