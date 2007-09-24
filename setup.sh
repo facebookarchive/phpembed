1c83703b71a0093f0ada14d998abaeed291b7c77
mvaradachari <mvaradachari@2c7ba8d8-a2f7-0310-a573-de162e16dcc7>
2007-09-24 07:43:49 +0000

    updating include paths etc
    git-svn-id: svn+ssh://tubbs/svnroot/projects/phpembed/trunk@60854 2c7ba8d8-a2f7-0310-a573-de162e16dcc7

--- setup.sh
+++ setup.sh
@@ -1,21 +1,18 @@
 #!/bin/sh
 # $Id$
 
-# Script to set up environment.
+# Script to set up compilation environment.
 
-rm -rf NEWS README AUTHORS ChangeLog
-rm -rf ltmain.sh configure depcomp config.guess config.sub config.h.in autom4te.cache INSTALL COPYING missing install-sh aclocal.m4 
-rm -rf mkinstalldirs phpembedVersion.h config.log config.status stamp-h1 config.h libtool
-
-
-touch NEWS README AUTHORS ChangeLog
-
-echo "libtoolize..."
-LIBTOOLIZE=${LIBTOOLIZE:-libtoolize}
-$LIBTOOLIZE --automake
+LIBTOOLIZE=${LIBTOOLIZE:-`which libtoolize`}
+if test -f "${LIBTOOLIZE}"; then
+   echo "libtoolize..."
+   $LIBTOOLIZE --automake
+else
+   echo " skipping libtoolize"
+fi
 
 echo "aclocal..."
-ACLOCAL=`which aclocal-1.9 || which aclocal19 || which aclocal-1.7 || which aclocal17 || which aclocal-1.5 || which aclocal15 || which aclocal || exit 1`
+ACLOCAL=${ACLOCAL:-aclocal}
 $ACLOCAL || exit 1
 
 echo "autoheader..."
@@ -23,8 +20,7 @@ AUTOHEADER=${AUTOHEADER:-autoheader}
 $AUTOHEADER || exit 1
 
 echo "automake..."
-AUTOMAKE=`which automake-1.9 || which automake-1.7 || which automake`
-#$AUTOMAKE --foreign --add-missing || $AUTOMAKE --gnu --add-missing || exit 1
+AUTOMAKE=${AUTOMAKE:-automake}
 $AUTOMAKE -a
 
 echo "autoconf..."
